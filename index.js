c1eebc8332b5007f4df23bf6bea1bfe6b36f3c94
ninh2003 <nguyendangninh2035@gmail.com>
2023-01-13 02:18:47 +0700

    ninh

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 const restaurant = {
     name: 'Calssico Italiano',
     location: 'Via Angelo Tavanti 23,Firenze,Italy',
-    categories: ['Italian','Pizzeria','Vegetarian','']
-}
+    categories: ['Italian','Pizzeria','Vegetarian','Organic'],
+    starterMenu: ['Focaccia','Bruschetta','Garlic Bread','Caprese Salad'],
+    mainMenu: ['Pizza','Pasta','Risotto'],
+};
